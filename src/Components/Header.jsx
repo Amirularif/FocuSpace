f2d078e7888cda08ce4d220fe2b134242fa17ff0
amirul <zainsyedahmad63@gmail.com>
2024-02-05 23:14:45 +0800

    design

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../css/Header.css'; 
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <header className="header">
-      <h1>FocuSpace</h1>
+      <h1 onClick={() => navigate('/')}>FocuSpace</h1>
       <button>Donation</button>
     </header>
   );
